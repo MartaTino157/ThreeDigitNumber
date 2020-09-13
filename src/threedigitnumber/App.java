732fabfc0d8d09a59b87d1fcd7d4eb185a527476
MartaTino <61226246+MartaTino157@users.noreply.github.com>
2020-09-13 18:04:47 +0300

    Home work

---
+++ src/threedigitnumber/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threedigitnumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alice
+ */
+public class App {
+    public void run(){
+        System.out.print("Введите любое трехзначное число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Число единиц: " + number%10);
+        System.out.println("Число десятков: " + number/10); /* или number/10%10, смотря что имеется ввиду под десятками*/
+        System.out.println("Сумма цифр = " + ((number/100)+((number/10)%10)+(number%10)));
+    }
+}
