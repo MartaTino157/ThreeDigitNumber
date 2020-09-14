651d5f64721426d00e973a63d0bf2be4656a4efe
MartaTino <61226246+MartaTino157@users.noreply.github.com>
2020-09-14 19:21:48 +0300

    Изменен счетчик десятков

--- src/threedigitnumber/App.java
+++ src/threedigitnumber/App.java
@@ -17,7 +17,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         System.out.println("Число единиц: " + number%10);
-        System.out.println("Число десятков: " + number/10); /* или number/10%10, смотря что имеется ввиду под десятками*/
+        System.out.println("Число десятков: " + number/10%10);
         System.out.println("Сумма цифр = " + ((number/100)+((number/10)%10)+(number%10)));
     }
 }
